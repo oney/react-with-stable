c70b42c3d226593da804443e7874d7daccce7655
Howard Yang <appdevoney@gmail.com>
2022-05-26 00:58:57 -0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# react-with-stable
---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import React, { ComponentType, MemoExoticComponent } from "react";
+
+export function withStable<P, T extends ComponentType<P>>(
+  stableKeys: string[],
+  Component: T
+): MemoExoticComponent<T> {
+  const Memo = React.memo(Component);
+  function ComponentWithStable(props: any) {
+    const extract = {};
+    for (const k in props) {
+      if (props.hasOwnProperty(k)) {
+        if (stableKeys.indexOf(k) !== -1) extract[k] = props[k];
+      }
+    }
+    const ref = React.useRef(extract);
+    React.useLayoutEffect(() => {
+      ref.current = extract;
+    });
+    const stable = React.useMemo(() => {
+      const stable = {};
+      for (const k in ref.current) {
+        stable[k] = (...args: any[]) => ref.current[k](...args);
+      }
+      return stable;
+    }, []);
+
+    return <Memo {...props} {...stable} />;
+  }
+
+  return ComponentWithStable;
+}
