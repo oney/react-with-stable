39c0309def321b9770bdc3c67ddda14ca30915ee
Howard Yang <appdevoney@gmail.com>
2022-05-26 22:14:48 -0700

    v0.1.3

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,29 @@
 import React, { ComponentType, MemoExoticComponent } from 'react';
 
-export function withStable<P, T extends ComponentType<P>>(
+export function withStable<T extends ComponentType<any>>(
   stableKeys: string[],
   Component: T
 ): MemoExoticComponent<T> {
   const Memo = React.memo(Component);
   function ComponentWithStable(props: any) {
-    const extract: any = {};
-    for (const k in props) {
-      if (props.hasOwnProperty(k)) {
-        if (stableKeys.indexOf(k) !== -1) extract[k] = props[k];
-      }
-    }
-    const ref = React.useRef(extract);
+    const propsRef = React.useRef(props);
     React.useLayoutEffect(() => {
-      ref.current = extract;
+      propsRef.current = props;
     });
-    const stable = React.useMemo(() => {
-      const stable: any = {};
-      for (const k in ref.current) {
-        stable[k] = (...args: any[]) => ref.current[k](...args);
-      }
-      return stable;
-    }, []);
+    const cache = React.useMemo(() => ({} as any), []);
 
-    return <Memo {...props} {...stable} />;
+    const stable = {} as any;
+    for (const k in props) {
+      if (!props.hasOwnProperty(k)) continue;
+      if (typeof props[k] !== 'function' || stableKeys.indexOf(k) === -1) {
+        stable[k] = props[k];
+        continue;
+      }
+      if (!cache[k])
+        cache[k] = (...args: any[]) => propsRef.current[k](...args);
+      stable[k] = cache[k];
+    }
+    return <Memo {...stable} />;
   }
 
   return ComponentWithStable as any;
